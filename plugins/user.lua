0d59880592b028193db0f9187e38d31e64407733
Firmino Veras <firmino.veras@live.com>
2023-07-12 22:43:51 -0300

    New logo and LSP configs

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,9 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup {}
+    end,
+  },
 }
